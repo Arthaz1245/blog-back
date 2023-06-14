76cdafb8841de0357bd0f7937da0f0a0c399b0d3
Arthaz1245 <sergiomendozap96@gmail.com>
2023-06-14 00:27:40 -0500

    cambios cv

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,32 +1,34 @@
 const Post = require("../models/Post");
-const User = require("../models/User");
+
 const fs = require("fs-extra");
 const { uploadImage } = require("../utils/Cloudinary");
 
 const createPost = async (req, res) => {
   try {
-    const { title, author, content, category } = req.body;
+    const { title, author, userId, content, category } = req.body;
     // const user = await User.findById(author);
     // if (user) {
     const post = new Post({
       title,
       author,
+      userId,
       content,
       category,
     });
 
     if (req.files?.image) {
-      const result = await uploadImage(req.files.image.tempFilePath);
+      const result = await uploadImage(req.files.image?.tempFilePath);
       post.image = {
         public_id: result.public_id,
         secure_url: result.secure_url,
       };
-      await fs.unlink(req.files.image.tempFilePath);
+
+      await fs.unlink(req.files.image?.tempFilePath);
     }
+    console.log(post);
+    const response = await post.save();
 
-    await post.save();
-
-    res.json(post);
+    res.json(response);
     // } else {
     //   return res.status(400).json("The author must be a register user");
     // }
@@ -36,7 +38,7 @@ const createPost = async (req, res) => {
 };
 const getAllPosts = async (req, res) => {
   try {
-    const posts = Post.find();
+    const posts = await Post.find();
     if (!posts) return res.status(400).json("There are not posts");
     res.status(200).json(posts);
   } catch (error) {
@@ -44,10 +46,10 @@ const getAllPosts = async (req, res) => {
     res.status(500).json(error);
   }
 };
-const getPostById = (req, res) => {
+const getPostById = async (req, res) => {
   const { id } = req.params;
   try {
-    const post = Post.findById(id);
+    const post = await Post.findById(id);
     if (!post) return res.status(400).json("This post does not exist");
     res.status(200).json(post);
   } catch (error) {
